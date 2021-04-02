8e7ff65d393bd64de52956a2bfe5af5e9dbda0c3
djonko <prevostdjonko@yahoo.fr>
2021-04-02 19:55:08 -0400

    step 4

--- src/test/java/com/example/devops/web/WelcomeControllerTest.java
+++ src/test/java/com/example/devops/web/WelcomeControllerTest.java
@@ -1,11 +1,40 @@
 package com.example.devops.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = WelcomeController.class)
+@WebAppConfiguration
 class WelcomeControllerTest {
 
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
     @Test
-    void welcome() {
+    void welcome() throws Exception {
+        //fail("Not yet implemented");
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("course", containsString("Devops")));
     }
 }
