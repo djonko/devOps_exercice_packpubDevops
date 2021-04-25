f39c3b0f30efe36305aa3d0f7d3b77f8cfc3a7d1
djonko <prevostdjonko@yahoo.fr>
2021-04-24 22:31:05 -0400

    1.3.4

--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -4,24 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("/")
+    @GetMapping(value = "/")
     public String welcome(Model model) {
         logger.info("Processing index request");
         model.addAttribute("course", "Devops");
         return "index";
     }
-    @RequestMapping("/wlcome_error")
-    public String welcomeToError(Model model) {
-        logger.info("Processing index request");
-        model.addAttribute("course", "Devops");
-        if(true)
-            throw new RuntimeException("sfd");
-        return "index";
-    }
+
 }
